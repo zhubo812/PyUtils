b8a303c44304c1490478fe581247f8d0e2c38334
Jackie <zhubo812@gmail.com>
2021-05-31 10:01:17 +0800

    fix error about sougolexlib page number

--- Crawler/SougoLexLib.py
+++ Crawler/SougoLexLib.py
@@ -34,23 +34,34 @@ class SouGSpider(object):
         fc = 0
         sc = 0
         tc = 0
+        namelist =['城市信息大全','电子游戏','自然科学']
+        subnamelist =['生物','化学','数学','物理','其它','天文学','气象学','海洋学']
         for ii in soup:
             fc += 1
-            print("Level 1 :" + ii.find(class_='dict_category_list_title').find('a').contents[0])
+            firstname = ii.find(class_='dict_category_list_title').find('a').contents[0]
+            if(firstname in namelist):
+                continue
+            print("Level 1 :" + firstname)
             first_dir = os.path.join(self.base_dir, ii.find(class_='dict_category_list_title').find('a').contents[0])
-            os.makedirs(first_dir, exist_ok=True)
+            os.makedirs(first_dir, exist_ok=True)#创建存储的路径
 
             for k in ii.find(class_='catewords').find_all('a'):
                 secondclass = k.contents[0]
                 second_url = self.base_url + "%s" % (k['href'])
                 print(" " * 4 + "Level 2 :" + secondclass)
+                if(secondclass in subnamelist):
+                    continue
                 print(second_url)
                 second_dir = os.path.join(first_dir, secondclass)
                 os.makedirs(second_dir, exist_ok=True)
 
                 sc += 1
                 soup2 = BeautifulSoup(requests.get(second_url).text, "html.parser")
-                totalpagenum = soup2.find(id='dict_page_list').find('ul').find_all('span')[-2].a.contents[0]
+                ullist= soup2.find(id='dict_page_list').find('ul').find_all('span')
+                if(len(ullist)==0):
+                    totalpagenum=1
+                else:
+                    totalpagenum = soup2.find(id='dict_page_list').find('ul').find_all('span')[-2].a.contents[0]
 
                 os.chdir(second_dir)
                 for pageind in range(1, int(totalpagenum) + 1):
